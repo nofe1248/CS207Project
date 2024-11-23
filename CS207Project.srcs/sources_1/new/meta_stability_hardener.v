8a46409004970d9f193c4a0ceb6cd6dc10d4fd34
Zhige Chen <zhigec_cpp@outlook.com>
2024-11-23 12:41:50 +0800

    meta hardener and baud rate generator

--- CS207Project.srcs/sources_1/new/meta_stability_hardener.v
+++ CS207Project.srcs/sources_1/new/meta_stability_hardener.v
@@ -1,6 +1,26 @@
 `timescale 1ns / 1ps
 
-module meta_stability_hardener(
+//a simple signal hardener
 
-    );
+module meta_stability_hardener(
+    input clk,
+    input rst,
+    input source_signal,
+    output reg hardened_signal
+);
+    reg meta_signal;
+    
+    always @(posedge clk)
+    begin
+        if (rst)
+        begin
+            meta_signal <= 1'b0;
+            hardened_signal <= 1'b0; 
+        end
+        else
+        begin
+            meta_signal <= source_signal;
+            hardened_signal <= meta_signal;
+        end
+    end
 endmodule
--- CS207Project.srcs/sources_1/new/uart_baud_rate_generator.v
+++ CS207Project.srcs/sources_1/new/uart_baud_rate_generator.v
@@ -1,6 +1,62 @@
 `timescale 1ns / 1ps
 
-module uart_baud_rate_generator(
+//a baud rate generator that generates a 16x baud enable
 
-    );
+module uart_baud_rate_generator(
+    input clk,
+    input rst,
+    output baud_x16_en
+);
+    //calculate the ceiling of log2 of n
+    function integer ceil_log2;
+        input [31:0] n;
+        
+        reg [31:0] temp;
+        begin
+            temp = n - 1;
+            for (ceil_log2 = 0; temp > 0; ceil_log2 = ceil_log2 + 1)
+                temp = temp >> 1;
+        end
+    endfunction 
+    
+    //parameters and local parameters:
+    parameter BAUD_RATE = 57_600;
+    parameter CLOCK_RATE = 100_000_000;
+    
+    localparam OVERSAMPLE_RATE = BAUD_RATE * 16;
+    localparam DIVIDER = (CLOCK_RATE + OVERSAMPLE_RATE / 2) / OVERSAMPLE_RATE;
+    localparam OVERSAMPLE_VALUE = DIVIDER - 1;
+    localparam CNT_WIDTH = ceil_log2(DIVIDER);
+    
+    //registers
+    reg [CNT_WIDTH-1:0] internal_cnt;
+    reg baud_x16_en_reg;
+    
+    //wires
+    wire [CNT_WIDTH-1:0] internal_cnt_minus_1;
+    
+    //body
+    always @(posedge clk)
+    begin
+        if (rst)
+        begin
+            internal_cnt <= OVERSAMPLE_VALUE;
+            baud_x16_en_reg <= 1'b0;
+        end
+        else
+        begin
+            baud_x16_en_reg <= (internal_cnt_minus_1 == {CNT_WIDTH{1'b0}});
+            
+            if (internal_cnt == {CNT_WIDTH{1'b0}})
+            begin
+                internal_cnt <= OVERSAMPLE_VALUE;
+            end
+            else
+            begin
+                internal_cnt <= internal_cnt_minus_1;
+            end
+        end
+    end
+    
+    assign baud_x16_en = baud_x16_en_reg;
 endmodule
